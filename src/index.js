e3a74a6bab88e1baa75a854005569be0ba47767c
Jeffry Gonzalez <jeff@hypertheory.com>
2019-01-28 18:51:09 +0000

    comments by Jeff

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import './styles.css';
-import { ready } from './utils';
+import { ready } from './utils'; // Glad you used this sucker.
 
+// JMG: THis is brilliant! *Great* idea! ;)
 export const tipInfo = {
     tipButtons: null,
     bill: null,
@@ -12,26 +13,24 @@ export const tipInfo = {
 }
 
 function setUp() {
-    tipInfo.bill = document.querySelector(".form-control");
+    tipInfo.bill = document.querySelector(".form-control"); // JMG This works, but is a little vague of a selector. Another form control added later could bonk you. I'd add an Id to the element.
     tipInfo.bill.oninput = updateTip;
     tipInfo.bill.addEventListener("keydown", function(c) {
         if ("e" == c.key) {
-          c.preventDefault();
+          c.preventDefault(); // JMG Well played.
         }
     });
 
 
-    tipInfo.tipButtons = document.querySelectorAll(".btn.btn-secondary");
+    tipInfo.tipButtons = document.querySelectorAll(".btn.btn-secondary"); // JMG - again, selecting on bootstrap classes is kinda fragile. 
     tipInfo.tipButtons[0].disabled = true;
-    tipInfo.tipPercent = tipInfo.tipButtons[0].innerText.slice(0,2);
-    tipInfo.tipButtons.forEach(b => {
-        b.addEventListener('click', tipPercentSelected);
-    });
+    tipInfo.tipPercent = tipInfo.tipButtons[0].innerText.slice(0,2); // JMG - cool hackering.
+    tipInfo.tipButtons.forEach(b =>  b.addEventListener('click', tipPercentSelected)); // JMG one liner FTW
 
-    tipInfo.currentTip = document.querySelector(".current-tip");
+    tipInfo.currentTip = document.querySelector(".current-tip"); // the dot prefix indicates a class. If you want to select a specific thing (not a class of things), use an ID and # selector. or document.getElementById('tacos')
     tipInfo.currentTip.innerHTML = `You are tipping ${tipInfo.tipPercent}%`;
 
-    tipInfo.output = document.querySelectorAll(".list-group-item");
+    tipInfo.output = document.querySelectorAll(".list-group-item"); // Same Same.
     updateOutput();
 }
 
@@ -51,7 +50,7 @@ function updateTip(){
     }
     else    {
         tipInfo.bill.classList.remove('error');
-        tipInfo.amountOfTip = (parseFloat(tipInfo.bill.value) * (parseInt(tipInfo.tipPercent)/100)).toFixed(2);
+        tipInfo.amountOfTip = (parseFloat(tipInfo.bill.value) * (parseInt(tipInfo.tipPercent)/100)).toFixed(2); // this parsing and rounding thing is something I'd create a helper function for.
         tipInfo.totalBill = (parseFloat(tipInfo.amountOfTip) + parseFloat(tipInfo.bill.value)).toFixed(2);
 
     }
@@ -59,10 +58,13 @@ function updateTip(){
 }
 
 function updateOutput() {
-    let formatBill = parseFloat(tipInfo.bill.value).toFixed(2);
+    let formatBill = parseFloat(tipInfo.bill.value).toFixed(2); // See there it is again! 
     if(isNaN(formatBill)){
         formatBill = "";
     }
+    // this is a nitpick, but I try to create as little content in the document from JS as possible.
+    // If there was an issue that said "We should say "Total Amount to be Paid:" instead of Total to Be Paid", most people
+    // would look at the html, not the JS to make that change. Here, they'd have to touch your JS.
     tipInfo.output.item(0).innerHTML =  `Bill Amount: $${formatBill}`;
     tipInfo.output.item(1).innerHTML =  `Tip Percentage: ${tipInfo.tipPercent}%`;
     tipInfo.output.item(2).innerHTML =  `Amount of tip: $${tipInfo.amountOfTip}`;
@@ -70,16 +72,10 @@ function updateOutput() {
 }
 
 function tipPercentSelected(){
-    tipInfo.tipButtons.forEach(b=>{
-        b.disabled = false;
-    });
+    tipInfo.tipButtons.forEach(b=> b.disabled = false );
     this.disabled = true;
     tipInfo.tipPercent = this.innerText.slice(0,2);
     tipInfo.currentTip.innerHTML = `You are tipping ${tipInfo.tipPercent}%`;
 
     updateTip()
 }
-
-// export const tipPercentSelectedFunction = tipPercentSelected;
-// export const setUpFunction = setUp;
-// export const updateTipFunction = updateTip
